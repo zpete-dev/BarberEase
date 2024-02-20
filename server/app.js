0e056a1d982073b9d59ed7f87adb0aad540225c1
zpeterson44 <zpete.dev@gmail.com>
2024-02-20 15:00:18 -0700

    changing db connection to be environment based

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const usersRoutes = require('./routes/users');
 
 const app = express();
 
-//app.use(cors());
+
 // Define CORS options
 const corsOptions = {
   origin: function (origin, callback) {
@@ -34,17 +34,35 @@ app.options('*', cors(corsOptions));
 // Apply CORS for all other requests
 app.use(cors(corsOptions));
 
-
 app.use(express.json());
+
+const fs = require('node:fs');
+let DB_PASSWORD = "";
+try {
+  const data = fs.readFileSync(process.env.DB_PASSWORD, 'utf8');
+  DB_PASSWORD = data;
+} catch (err) {
+  console.error("Error loading Secret file");
+}
+
+const DB_USER = process.env.DB_USER;
+//const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
+const DB_OPTIONS = process.env.DB_OPTIONS;
+
+const DB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}${DB_OPTIONS}`;
+
+//console.log(DB_URL);
 //TODO - CHANGE ALL HARD CODED ADDRESS REFERENCES
 // Connect to MongoDB
-//mongoose.connect('mongodb://localhost:27017/barberease', {
-mongoose.connect('mongodb://mongodb:27017/barberease', {
-useNewUrlParser: true,
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err));
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
 
 // API routes
 app.use('/api/bookings', bookingsRoutes);
