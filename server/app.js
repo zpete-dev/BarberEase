16af7c390a37ac5bfdf7c72315e7b18734d708bc
zpeterson44 <zpete.dev@gmail.com>
2024-03-07 11:46:14 -0700

    Production build configurations

--- server/app.js
+++ server/app.js
@@ -71,7 +71,7 @@ mongoose.connect(DB_URL, {
 app.use('/api/bookings', bookingsRoutes);
 app.use('/api/barbers', barbersRoutes);
 app.use('/api/sessions', sessionsRoutes);
-app.use('/api/users', usersRoutes); //This route is intentionally not always on
+//app.use('/api/users', usersRoutes); //This route is intentionally not always on
 
 // Root route
 app.get('/', (req, res) => {
